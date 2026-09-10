440186d90710f9a4620c86c40b74f5f67ce87216
pluque <pedro.luque@eynes.com.ar>
2026-09-09 21:56:48 -0300

    Configurar servidor Express con logging y pool de conexion MySQL

---
+++ backend/src/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const routes = require("./routes");
+const logger = require("./middlewares/logger");
+const { notFound, errorHandler } = require("./middlewares/errorHandler");
+
+const app = express();
+
+app.use(logger);
+app.use(express.json());
+
+app.use("/api", routes);
+
+app.use(notFound);
+app.use(errorHandler);
+
+module.exports = app;
